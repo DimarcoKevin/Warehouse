1b2b0dfea87fe802bf6f1603a7a677b121800498
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-05-29 17:25:28 -0400

    Search works now

--- Warehouse/Items.aspx.cs
+++ Warehouse/Items.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +9,25 @@ using System.Web.UI.WebControls;
 
 namespace Warehouse {
     public partial class Items : System.Web.UI.Page {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=Warehouse; Integrated Security=true");
         protected void Page_Load(object sender, EventArgs e) {
+            SqlDataAdapter SqlAdapter = new SqlDataAdapter("select * from dbo.items where name like ('%%')", con);
+            DataTable dt = new DataTable();
 
+            SqlAdapter.Fill(dt);
+
+            GridViewItems.DataSource = dt;
+            GridViewItems.DataBind();
+        }
+
+        protected void SearchButton_Click(object sender, EventArgs e) {
+            SqlDataAdapter SqlAdapter = new SqlDataAdapter("select * from dbo.items where name like ('%" + SearchBox.Text + "%')", con);
+            DataTable dt = new DataTable();
+
+            SqlAdapter.Fill(dt);
+
+            GridViewItems.DataSource = dt;
+            GridViewItems.DataBind();
         }
     }
 }

21fd2d898c3481788d5bde5c596c1797e16b5be0
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-05-29 19:03:39 -0400

    Completing basic design for create item page

--- Warehouse/CreateItem.aspx
+++ Warehouse/CreateItem.aspx
@@ -10,13 +10,27 @@
     <form id="form1" runat="server">
         <h1>Create new items</h1>
         <div>
-            <asp:Label ID="ItemLabel" runat="server">Item:</asp:Label>
+            <asp:Label ID="ItemLabel" runat="server">Item</asp:Label>
             <asp:TextBox ID="ItemTextBox" runat="server"></asp:TextBox>
         </div>
         <div>
-            <asp:Label ID="Color" runat="server">Color:</asp:Label>
+            <asp:Label ID="Color" runat="server">Color</asp:Label>
             <asp:DropDownList ID="ColorDD" runat="server" ></asp:DropDownList>
-          
+        </div>
+        <div>
+            <asp:Label ID="DescLabel" runat="server">Description</asp:Label>
+            <asp:TextBox ID="DescTextBox" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="MaxLabel" runat="server">Max per pallet</asp:Label>
+            <asp:TextBox ID="MaxTextBox" runat="server" TextMode="Number"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="PriceLabel" runat="server">Price ($)</asp:Label>
+            <asp:TextBox ID="PriceTextBox" runat="server" TextMode="Number"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="SubmitButton" runat="server" Text="Submit"></asp:Button>
         </div>
     </form>
 </body>
--- Warehouse/CreateItem.aspx.designer.cs
+++ Warehouse/CreateItem.aspx.designer.cs
@@ -56,5 +56,68 @@ namespace Warehouse {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList ColorDD;
+
+        /// <summary>
+        /// DescLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DescLabel;
+
+        /// <summary>
+        /// DescTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox DescTextBox;
+
+        /// <summary>
+        /// MaxLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label MaxLabel;
+
+        /// <summary>
+        /// MaxTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox MaxTextBox;
+
+        /// <summary>
+        /// PriceLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label PriceLabel;
+
+        /// <summary>
+        /// PriceTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox PriceTextBox;
+
+        /// <summary>
+        /// SubmitButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SubmitButton;
     }
 }

f3d9418384ccfc21aefc94f34b18c8fdaaf84d5e
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-05-29 18:58:42 -0400

    Adding CreateItem page and starting design

---
+++ Warehouse/CreateItem.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateItem.aspx.cs" Inherits="Warehouse.CreateItem" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Create new items</h1>
+        <div>
+            <asp:Label ID="ItemLabel" runat="server">Item:</asp:Label>
+            <asp:TextBox ID="ItemTextBox" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="Color" runat="server">Color:</asp:Label>
+            <asp:DropDownList ID="ColorDD" runat="server" ></asp:DropDownList>
+          
+        </div>
+    </form>
+</body>
+</html>
---
+++ Warehouse/CreateItem.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Warehouse {
+    public partial class CreateItem : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            ColorDD.Items.Add("Red");
+            ColorDD.Items.Add("Blue");
+            ColorDD.Items.Add("Green");
+            ColorDD.Items.Add("Yellow");
+            ColorDD.Items.Add("Pink");
+            ColorDD.Items.Add("Purple");
+            ColorDD.Items.Add("Orange");
+            ColorDD.Items.Add("Brown");
+            ColorDD.Items.Add("Black");
+            ColorDD.Items.Add("White");
+        }
+    }
+}

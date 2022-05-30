294233774b0907b3f8a4653baefa3cbab1773754
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-05-29 21:19:40 -0400

    Adding Signup page and logic

---
+++ Warehouse/Signup.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Signup.aspx.cs" Inherits="Warehouse.Signup" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Signup</title>
+</head>
+<body>
+    <form id="form1" runat="server">   
+        <h2>Signup Page</h2>
+        <div>
+            <asp:Label ID="username_label" runat="server" Text="Username:"></asp:Label>
+            <asp:TextBox ID="username" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="password_label" runat="server" Text="Password:"></asp:Label>
+            <asp:TextBox ID="password" runat="server" TextMode="Password"></asp:TextBox>
+        </div>
+        <asp:Button runat="server" ID="signup_button" Text="Signup" OnClick="signup_button_Click" />
+        <asp:HyperLink id="login_hyperlink" NavigateUrl="../Login.aspx" Text="Login" runat="server"/> 
+        <br />
+        <asp:Label ID="signup_message" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Warehouse/Signup.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Warehouse {
+    public partial class Signup : System.Web.UI.Page {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=Warehouse; Integrated Security=true");
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void signup_button_Click(object sender, EventArgs e) {
+            SqlDataAdapter SqlAdapter = new SqlDataAdapter("select * from dbo.users where username = '" + username.Text + "'", con);
+            DataTable dt = new DataTable();
+
+            SqlAdapter.Fill(dt);
+            if (dt.Rows.Count > 0) {
+                signup_message.Text = "Error, that user name is already used.";
+            } else {
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO USERS(username, password, role) VALUES(@Username, @Password, @Role)", con);
+
+                cmd.Parameters.Add("@Username", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Password", SqlDbType.VarChar);
+                cmd.Parameters.Add("@Role", SqlDbType.VarChar);
+
+                cmd.Parameters["@Username"].Value = username.Text;
+                cmd.Parameters["@Password"].Value = password.Text;
+                cmd.Parameters["@Role"].Value = "user";
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                signup_message.Text = "You have successfully signed up! Welcome " + username.Text + "!";
+            }
+        }
+    }
+}

6ebdcf1c3d602d7af2533287f91e1c79da591fd9
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-06-19 22:01:57 -0400

    Setting up query template for search by item page

--- Warehouse/SearchByItem.aspx.cs
+++ Warehouse/SearchByItem.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,8 +9,19 @@ using System.Web.UI.WebControls;
 
 namespace Warehouse {
     public partial class SearchByItem : System.Web.UI.Page {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=Warehouse; Integrated Security=true");
+
         protected void Page_Load(object sender, EventArgs e) {
 
+        }
+
+        protected void SearchButton_Click(object sender, EventArgs e) {
+            String select = "";
+
+            SqlDataAdapter SqlAdapter = new SqlDataAdapter("", con);
+            DataTable dt = new DataTable();
+
+            SqlAdapter.Fill(dt);
         }
     }
 }

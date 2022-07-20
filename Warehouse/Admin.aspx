8d16d646d4fc3dab074f1b0e903c5057990fb954
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2022-07-20 19:43:47 -0400

    Adding checks for a regular user accessing admin page

--- Warehouse/Admin.aspx
+++ Warehouse/Admin.aspx
@@ -5,6 +5,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>Admin Panel</title>
+    <script>
+        function restricted() {
+            alert("You do not have access to this page");
+        }
+
+    </script>
 </head>
 <body>
     <form id="form1" runat="server">
--- Warehouse/Admin.aspx.cs
+++ Warehouse/Admin.aspx.cs
@@ -5,13 +5,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Threading;
 using Warehouse.Variables;
 
 namespace Warehouse {
     public partial class Admin : System.Web.UI.Page {
         SqlConnection con = new SqlConnection(GlobalVariables.connectionString);
+        public string user = GlobalVariables.user;
+        public string role = GlobalVariables.role;  
         protected void Page_Load(object sender, EventArgs e) {
 
+            // checking if role is anything but admin
+            if (!role.Equals("admin")) {
+                Response.Redirect("Home.aspx");
+            }
         }
+
     }
 }
